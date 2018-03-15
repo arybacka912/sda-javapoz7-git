d27158f8e181309cb84cd603cadb40ceecabc1b7
Anka <arybacka912@wp.pl>
2018-03-15 18:55:56 +0100

    Random Generator

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -1,15 +1,17 @@
 package sda;
 
 import sda.calculator.Calculator;
+import sda.calculator.RandomGenerato;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello word");
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(5,10));
-        System.out.println(calculator.substract(5,10));
-        System.out.println(calculator.divide1(5,10));
-        System.out.println(calculator.multiply(5,10));
+        RandomGenerato randomGenerato = new RandomGenerato();
+        System.out.println(calculator.sum(randomGenerato.getSmallNumber(), randomGenerato.getBigNumber()));
+        System.out.println(calculator.substract(randomGenerato.getSmallNumber(), randomGenerato.getBigNumber()));
+        System.out.println(calculator.divide1(randomGenerato.getSmallNumber(), randomGenerato.getBigNumber()));
+        System.out.println(calculator.multiply(randomGenerato.getSmallNumber(), randomGenerato.getBigNumber()));
 
 
 
