d025d84aad2a232bc051ba5de4cf55d841a41612
Anka <arybacka912@wp.pl>
2018-03-15 18:32:31 +0100

    calculator basic features

--- src/sda/Application.java
+++ src/sda/Application.java
@@ -1,7 +1,11 @@
 package sda;
 
+import sda.calculator.Calculator;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5,10));
     }
 }
